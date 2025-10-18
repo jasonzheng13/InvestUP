f0a8aa3001db902f5d70bf9ad683ee6b9eb0b1f9
jasonzheng13 <117415854+jasonzheng13@users.noreply.github.com>
2025-10-18 17:23:55 -0400

    Added React Route, updated postcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
-import Home from './pages/Home.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Welcome from './pages/Welcome.jsx';
+import Search from './pages/Search.jsx';
+import Info from './pages/Info.jsx';
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
-
-
 
